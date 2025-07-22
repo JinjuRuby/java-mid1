a0fe2b98ce0ef2ade97774c79a948440d7b0d2b1
JinjuRuby <ronnie0304@naver.com>
2025-07-22 18:14:43 +0900

    study exception handling

---
+++ src/exception/ex1/MainV1.java
@@ -0,0 +1,24 @@
+package exception.ex1;
+
+import java.util.Scanner;
+
+public class MainV1 {
+
+    public static void main(String[] args) {
+
+        //NetworkSeviceV1_1 networkSevice = new NetworkSeviceV1_1();
+        NetworkSeviceV1_2 networkSevice = new NetworkSeviceV1_2();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송할 문자: ");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            networkSevice.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다.");
+    }
+}
