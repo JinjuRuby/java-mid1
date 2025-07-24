3dde41a355c28e178f69ee50f3197780f16b98bc
JinjuRuby <ronnie0304@naver.com>
2025-07-24 16:15:42 +0900

    studied nested classes

--- src/nested/inner/ex1/Engine.java
+++ src/nested/inner/ex1/Engine.java
@@ -12,7 +12,5 @@ public class Engine {
     public void start() {
         System.out.println("충전 레벨 확인: " + car.getChargeLevel());
         System.out.println(car.getModel() + "의 엔진을 구동합니다.");
-
-
     }
 }
